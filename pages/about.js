f56e2d365bc41e17abdfa3ddc78e5cd5b9cb3d9a
Vyver7952 <danielbudip789077@gmail.com>
2023-01-05 23:15:29 +0700

    Index Page

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 import React, { Component } from "react";
 
 export class about extends Component {
@@ -8,26 +8,28 @@ export class about extends Component {
         <Head>
           <title>About Page</title>
         </Head>
-        <div>
-          <h1>About</h1>
-          <ul>
-            <li className="item" id="id1">
-              Item 1
-            </li>
-            <li className="item" id="id2">
-              Item 2
-            </li>
-            <li className="item" id="id3">
-              Item 3
-            </li>
-            <li className="item" id="id4">
-              Item 4
-            </li>
-            <li className="item" id="id5">
-              Item 5
-            </li>
-          </ul>
-        </div>
+        <main>
+          <div>
+            <h1 className="text-3xl">About</h1>
+            <ul>
+              <li className="item" id="id1">
+                Item 1
+              </li>
+              <li className="item" id="id2">
+                Item 2
+              </li>
+              <li className="item" id="id3">
+                Item 3
+              </li>
+              <li className="item" id="id4">
+                Item 4
+              </li>
+              <li className="item" id="id5">
+                Item 5
+              </li>
+            </ul>
+          </div>
+        </main>
       </>
     );
   }
